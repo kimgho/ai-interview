d7c5241e747dcc4c94cd4e9c999e2e3e38ab9ee2
kimgho <kgh67811@knu.ac.kr>
2025-05-08 02:31:00 +0900

    feat: implement ChattingPage layout

--- src/pages/ChattingPage.tsx
+++ src/pages/ChattingPage.tsx
@@ -1,9 +1,22 @@
-import ChatInput from "@/components/chat/ChatInput";
+import ChatInput from "@/components/chat/ChatInput"
+import ChatSidebar from "@/components/chat/ChatSidebar"
+import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 
 const ChattingPage = () => {
     return (
-        <ChatInput />
+        <SidebarProvider>
+            <ChatSidebar />
+            <SidebarInset className="flex flex-col">
+                <SidebarTrigger className="-ml-1" />
+                <div className="flex-1 overflow-auto p-4">
+                    <div className="min-h-[calc(100vh-8rem)]"></div>
+                </div>
+                <div className="border-t p-4">
+                    <ChatInput />
+                </div>
+            </SidebarInset>
+        </SidebarProvider>
     )
 }
 
-export default ChattingPage;
+export default ChattingPage
