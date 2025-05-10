edc7ebe6d19834786cbb92913c463fc4fe5c0359
kimgho <kgh67811@knu.ac.kr>
2025-05-11 03:05:00 +0900

    feat: seperate UI structure from ChatPage.tsx

--- src/pages/ChattingPage.tsx
+++ src/pages/ChattingPage.tsx
@@ -1,21 +1,11 @@
-import ChatInput from "@/components/chat/ChatInput"
-import ChatSidebar from "@/components/chat/ChatSidebar"
-import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import ChatLayout from "@/components/chat/ChatLayout"
+import { ChatProvider } from "@/contexts/ChatContext"
 
 const ChattingPage = () => {
     return (
-        <SidebarProvider>
-            <ChatSidebar />
-            <SidebarInset className="flex flex-col">
-                <SidebarTrigger className="-ml-1" />
-                <div className="flex-1 overflow-auto p-4">
-                    <div className="min-h-[calc(100vh-8rem)]"></div>
-                </div>
-                <div className="border-t p-4">
-                    <ChatInput />
-                </div>
-            </SidebarInset>
-        </SidebarProvider>
+        <ChatProvider>
+            <ChatLayout />
+        </ChatProvider>
     )
 }
 
