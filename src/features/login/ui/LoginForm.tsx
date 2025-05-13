ec8499aaab748489234a191a760c6c5255204c4d
kimgho <kgh67811@knu.ac.kr>
2025-05-13 21:33:57 +0900

    feat: enhance LoginForm with useLogin

--- src/features/login/ui/LoginForm.tsx
+++ src/features/login/ui/LoginForm.tsx
@@ -1,15 +1,21 @@
-import InputField from "../../../entities/auth/ui/InputField"
+import InputField from "@/entities/auth/ui/InputField"
 import LoginButton from "@/entities/auth/ui/LoginButton"
+import { useLogin } from "../model/useLogin"
 
 
 const LoginForm = () => {
+    const { error, fieldError, formData, handleChange, handleSubmit, loading } = useLogin();
     return (
-        <div className="space-y-4">
+        <form onSubmit={handleSubmit} className="space-y-4">
             <InputField
                 id="email"
                 label="이메일"
                 type="email"
                 placeholder="name@example.com"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                error={fieldError.email}
             />
 
             <InputField
@@ -21,10 +27,15 @@ const LoginForm = () => {
                         비밀번호 찾기
                     </a>
                 }
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                error={fieldError.password}
             />
 
-            <LoginButton>로그인</LoginButton>
-        </div>
+            <LoginButton disabled={loading}>{loading ? '로그인중입니다..' : '로그인'}</LoginButton>
+            {error && <p className="text-red-500 text-sm mt-2">{error}</p>}
+        </form>
     )
 }
 
